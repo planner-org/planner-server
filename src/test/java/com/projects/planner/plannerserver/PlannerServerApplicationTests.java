09de461325da20bea38ff361f858bd54fa0369ac
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2022-09-06 15:49:11 +0700

    Initial commit. Added main services: config, server and gateway.

---
+++ src/test/java/com/projects/planner/plannerserver/PlannerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.planner.plannerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
